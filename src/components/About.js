2318cdb0aa9c115b56f122a19aefa329a6883351
Lucy Wang <wang.lucy@hotmail.com>
2023-04-03 23:36:30 -0400

    setup home page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import "./About.css"
 
 function About() {
   return (
-    <div>About</div>
+    <div id="about-page">
+      <div id="intro-section">
+         <h1 class="title">Industry 4.0</h1>
+      </div>
+
+      <div id="about-section">
+        <br></br>
+        <h1 class="left-title">About Us</h1>
+        <h1 class="right-title">Our Mission</h1>
+        <h1 class="left-title">Our Vision</h1>
+      </div>
+     
+    </div>
   )
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,12 +5,6 @@ import About from '../About';
 function Home() {
     return(
         <>
-        <h1>HOME PAGE</h1>
-        <p>1. Logo/name</p>
-        <p>2. About Us</p>
-        <p>3. Mission/Values?</p>
-        <p>4. Sponsors</p>
-        <br />
         <About />
         </>
     );
