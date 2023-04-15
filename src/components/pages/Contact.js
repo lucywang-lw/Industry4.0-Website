e6f2f21ff2afbf4340d34bdc0f2f2a179809cd6a
Lucy Wang <wang.lucy@hotmail.com>
2023-04-14 21:01:34 -0400

    send email through form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,14 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Contact.css"
 
-
-// const submission = (event) => {
-//   event.preventDefault();
-//   alert('Done')
-  
-// }
-
 function Contact() {
+  const [status, setStatus] = useState("Submit");
   const [name, setName] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [message, setMessage] = React.useState('');
@@ -25,10 +19,24 @@ function Contact() {
     setMessage(event.target.value);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    alert(email + ' ' + message + ' ' + name);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    let details = {
+      name: name,
+      email: email,
+      message: message,
+    };
+    let response = await fetch("http://localhost:5500/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
   };
 
   return (
@@ -36,31 +44,31 @@ function Contact() {
       <br></br>
     <h1 className="title">Contact Us</h1>
       <h1>Socials</h1>
-      <div class="text-medium"> Have a question about I4.0? Reach out to us through our socials. We'll be happy to answer!</div>
+      <div className="text-medium"> Have a question about I4.0? Reach out to us through our socials. We'll be happy to answer!</div>
 
       <div className='socials'>            
         <a className='social' href="https://www.instagram.com/i4competition/"> 
           <i className="fas fa fa-instagram icon"></i> 
-          <span class="username">@i4competition</span>
+          <span className="username">@i4competition</span>
         </a>
       
         <a className='social' href="https://www.gmail.com"> 
           <i className="fas fa fa-google icon"></i>
-          <span class="username">i4@gmail.com</span>
+          <span className="username">i4@gmail.com</span>
         </a>
 
         <a className='social' href="https://www.gmail.com"> 
           <i className="fas fa fa-google icon"></i>
-          <span class="username">i4@gmail.com</span>
+          <span className="username">i4@gmail.com</span>
         </a>
 
         <a className='social' href="https://www.gmail.com"> 
           <i className="fas fa fa-google icon"></i>
-          <span class="username">i4@gmail.com</span>
+          <span className="username">i4@gmail.com</span>
         </a>
         <a className='social' href="https://www.gmail.com"> 
           <i className="fas fa fa-google icon"></i>
-          <span class="username">i4@gmail.com</span>
+          <span className="username">i4@gmail.com</span>
         </a>
       </div>
 
@@ -70,15 +78,15 @@ function Contact() {
         <h1>Send Us a Message</h1>
         <form className= "e-form" onSubmit={handleSubmit}>
           <label className= "form-labels"> Name </label>
-          <input className= "form-input" type = "text" value={name} onChange={handleName}/>
+          <input className= "form-input" type = "text" value={name} onChange={handleName} required />
 
           <label className= "form-labels"> Email </label>
-          <input className= "form-input" type = "text" value={email} onChange={handleEmail}/>
+          <input className= "form-input" type = "text" value={email} onChange={handleEmail} required />
 
           <label className= "form-labels"> Message </label>
-          <input className= "form-input" type = "text" value={message} onChange={handleMessage} style = {{paddingBottom: "50px"}}/>
+          <input className= "form-input" type = "text" value={message} onChange={handleMessage} style = {{paddingBottom: "50px"}} required />
           
-          <button className='submit' type="submit">Submit</button>
+          <button className='submit' type="submit">{status}</button>
         </form>
       </div>
     </div>
