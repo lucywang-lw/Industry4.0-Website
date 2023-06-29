b45711dd1f2addb282db7b2c2cae8584daf03527
joshli52 <67028612+joshli52@users.noreply.github.com>
2023-06-29 16:28:24 -0400

    added past sponsors

--- src/components/pages/Sponsors.js
+++ src/components/pages/Sponsors.js
@@ -9,33 +9,31 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import 'swiper/css/keyboard';
 import toyota from '../images/toyota.png';
+import UW from '../images/UW.jpeg';
+import Maplesoft from '../images/Maplesoft.png';
+import UWMSCI from '../images/UWMSCI.png';
 import './Sponsors.css';
   //list of faqs, mapped later
 const SponsorList = [
   {
-    name: "toyota",
+    name: "Toyota",
     image: toyota,
-    text: "details about the sponsor here",
+    text: "Toyota is a renowned Japanese automaker. With a strong emphasis on quality, efficiency, and innovation, Toyota has established itself as a leader in the automotive industry.",
   },
   {
-    name: "toyota",
-    image: toyota,
-    text: "details about the sponsor here",
+    name: "Maplesoft",
+    image: Maplesoft,
+    text: "Maplesoft is a leading software company specializing in advanced mathematical and analytical solutions. With their flagship product, Maple, they provide a powerful platform for mathematical modeling, simulation, and analysis, catering to a wide range of industries and academic institutions worldwide.",
   },
   {
-    name: "test",
-    image: toyota,
-    text: "details about the sponsor here",
+    name: "University of Waterloo",
+    image: UW,
+    text: "Renowned for its strong focus on cooperative education, entrepreneurship, and innovation, the university offers a wide range of academic programs across various disciplines.",
   },
   {
-    name: "toyota",
-    image: toyota,
-    text: "details about the sponsor here",
-  },
-  {
-    name: "test",
-    image: toyota,
-    text: "details about the sponsor here",
+    name: "Management Sciences Department",
+    image: UWMSCI,
+    text: "Known for its unique blend of business, mathematics, and engineering principles, the program equips students with the analytical skills and strategic thinking necessary to solve complex problems and make informed decisions in a wide range of industries.",
   },
 ]
 
