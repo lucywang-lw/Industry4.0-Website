fddb818306cad2f8fde6e9b539fe7965ea8a8a65
joshli52 <67028612+joshli52@users.noreply.github.com>
2023-11-23 11:43:04 -0500

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,24 @@ function Footer() {
     <div className='footer'>
       <img className="logo" src={logoNoText} alt="logo" />
       Industry 4.0
+      <div className='socials-footer'>            
+        <a className='social-footer' href="https://www.instagram.com/i4competition/"> 
+          <i className="fas fa fa-instagram icon"></i> 
+        </a>
+      
+        <a className='social-footer' href="https://www.linkedin.com/company/i4competition/"> 
+          <i class="fa-brands fa-linkedin-in icon"></i>
+        </a>
+
+        <a className='social-footer' href="mailto: industry4team@uwaterloo.ca"> 
+          <i class="fa-regular fa-envelope icon"></i>
+        </a>
+
+        <a className='social-footer' href="https://discord.gg/8b5PyAfC"> 
+        <i class="fa-brands fa-discord icon"></i>
+        </a>
+      </div>
     </div>
   )
 }
-
 export default Footer
